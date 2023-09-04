225aca068e6442eeb4ba3e557f0059806945e0cd
RikardoFlamenguista <lhss2003@gmail.com>
2023-09-04 16:10:29 -0300

    pull chao rascunho

---
+++ Assets/Scipts/Ground.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+   /* public ModuloPista[] listaModulos;
+
+    public int tamanhoInicial;
+    ModuloPista copiado;
+    static public Pista pista;
+    
+   
+
+
+    public List<ModuloPista> poolModulosPista;
+
+    public float velocidade;
+    private Vector3 posicaoPista;
+    Vector3 posicao;
+
+    private void MovimentarPista()
+    {
+        posicaoPista.z -= velocidade;
+        this.transform.position = posicaoPista;
+    }
+
+    private void GerarPista()
+    {
+        int ModuloSorteado = Random.Range(1, listaModulos.Length);
+        posicao = copiado.connector.transform.position;
+
+        copiado = Instantiate(listaModulos[ModuloSorteado], this.transform);
+        copiado.transform.position = posicao;
+
+    }
+
+    
+    void Start()
+    {
+        InvokeRepeating("MovimentarPista", 2, 0.055f);
+        posicaoPista = this.transform.position;
+       
+        pista = this;
+
+        
+        copiado = Instantiate(listaModulos[0], this. transform);
+
+        
+            GerarPista();
+        
+    }
+
+    */
+}
