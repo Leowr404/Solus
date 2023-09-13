29ce16c13b6cb803bcc70d09ae51ed5d92c41b24
Leowr404 <Leonardounity404@gmail.com>
2023-09-13 07:17:43 -0300

    fix GameController

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class GameController : MonoBehaviour
 {
+
+
+    public static GameController instance;
+
     // Start is called before the first frame update
     void Start()
     {
