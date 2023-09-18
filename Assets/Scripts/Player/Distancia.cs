3698457b45cadbfbef9a1f48e10ec46bb888fbcd
Leowr404 <Leonardounity404@gmail.com>
2023-09-18 13:24:08 -0300

    Add Contador De Distancia

---
+++ Assets/Scripts/Player/Distancia.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Distancia : MonoBehaviour
+{
+    public Transform player; // Referência ao transform do jogador
+    public Text distanceText; // Referência ao objeto de texto
+
+    private float initialPlayerPosition; // Posição inicial do jogador
+
+    void Start()
+    {
+        // Registra a posição inicial do jogador
+        initialPlayerPosition = player.position.z;
+    }
+
+    void Update()
+    {
+        // Calcule a distância percorrida pelo jogador
+        float distance = player.position.z - initialPlayerPosition;
+
+        // Atualize o texto do marcador de distância
+        distanceText.text = "Distância: " + distance.ToString("F0") + "m"; // "F0" formata para um número inteiro
+    }
+}
