299c8514f8e34c5bcf4a0f09a21df66a75348b47
Leowr404 <Leonardounity404@gmail.com>
2023-10-15 08:19:56 -0300

    Fix Error AudioManager

--- Assets/Scripts/AudioController.cs
+++ Assets/Scripts/AudioController.cs
@@ -37,17 +37,21 @@ public class AudioController : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
-
     }
     void Start()
     {
-        // Carregar os valores dos PlayerPrefs quando o jogo é iniciado
-        float savedSFXVolume = PlayerPrefs.GetFloat(SFXVolumeKey, 1.0f); // 1.0f é o valor padrão
+        float savedSFXVolume = PlayerPrefs.GetFloat(SFXVolumeKey, 1.0f);
         float savedMusicVolume = PlayerPrefs.GetFloat(MusicVolumeKey, 1.0f);
 
-        SFXSlider.value = savedSFXVolume;
-        MusicSlider.value = savedMusicVolume;
+        if (SFXSlider != null)
+        {
+            SFXSlider.value = savedSFXVolume;
+        }
+
+        if (MusicSlider != null)
+        {
+            MusicSlider.value = savedMusicVolume;
+        }
 
         // Configurar os volumes iniciais
         SetMusicSFX();
@@ -56,21 +60,27 @@ public class AudioController : MonoBehaviour
 
     public void SetMusicSFX()
     {
-        float volume = SFXSlider.value;
-        mixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
-        SFXSlider.value = volume;
-        PlayerPrefs.SetFloat(SFXVolumeKey, volume);
-        PlayerPrefs.Save();
+        if (mixer != null && SFXSlider != null)
+        {
+            float volume = SFXSlider.value;
+            mixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+            SFXSlider.value = volume;
+            PlayerPrefs.SetFloat(SFXVolumeKey, volume);
+            PlayerPrefs.Save();
+        }
 
     }
 
     public void SetMusic()
     {
-        float volume = MusicSlider.value;
-        mixer.SetFloat("Musica", Mathf.Log10(volume) * 20);
-        MusicSlider.value = volume;
-        PlayerPrefs.SetFloat(MusicVolumeKey, volume);
-        PlayerPrefs.Save();
+        if (mixer != null && MusicSlider != null)
+        {
+            float volume = MusicSlider.value;
+            mixer.SetFloat("Musica", Mathf.Log10(volume) * 20);
+            MusicSlider.value = volume;
+            PlayerPrefs.SetFloat(MusicVolumeKey, volume);
+            PlayerPrefs.Save();
+        }
 
     }
 }
